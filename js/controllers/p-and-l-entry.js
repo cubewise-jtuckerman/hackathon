ef49bce9827c8174cb19cf32eb61dd364d3b8c7d
cw-leung <cleung@cubewise.com>
2018-03-08 17:22:06 +0800

    subnm done

--- js/controllers/p-and-l-entry.js
+++ js/controllers/p-and-l-entry.js
@@ -1,4 +1,4 @@
-app.controller('pandlentryCtrl', ['$scope', '$rootScope', '$log', '$tm1Ui', function($scope, $rootScope, $log, $tm1Ui) {
+app.controller('pandlentryCtrl', ['$scope', '$rootScope', '$log', '$tm1Ui', '$location', function($scope, $rootScope, $log, $tm1Ui, $location) {
    /*
     *     defaults.* are variables that are declared once and are changed in the page, otherwise known as constants in programming languages
     *     lists.* should be used to store any lists that are used with ng-repeat, i.e. tm1-ui-element-list
@@ -8,9 +8,36 @@ app.controller('pandlentryCtrl', ['$scope', '$rootScope', '$log', '$tm1Ui', func
     *     For more information: https://github.com/cubewise-code/canvas-best-practice
     */
     
-    $scope.defaults = {};
-    $scope.selections = {};
+    $scope.defaults = {
+        version: '',
+        year: '',
+        region: '',
+        dept: ''
+    };
+    $scope.selections = {
+        year: $scope.defaults.year
+    };
     $scope.lists = {};
     $scope.values = {};
+
+    $scope.init = function() {
+        //set default for other selection from User settings
+        _.forEach(['Year'], function(dim) {
+            if ( $location.search()[dim] ) {
+                $scope.defaults[dim] = $location.search()[dim];
+            } else {
+                $tm1Ui.cellGet('dev', 'System User Settings', 'Admin', dim, 'String').then(function(data){ 
+                    if ( data ) { $scope.defaults[dim] = data.Value;}
+                });
+            };
+        });
+    }
+
+    $scope.setUrl = function(dim, elem) {
+        $location.search(dim, elem);
+        $tm1Ui.cellPut ( elem, 'dev', 'System User Settings', 'Admin', dim, 'string');
+    }
     
+    $scope.init();
+
 }]);
